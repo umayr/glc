4ac3d001a55e6f7df3afbfa6e6bae354f46a5d0d
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-15 17:36:35 +0500

    adding colours on load

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,28 +2,23 @@
     'use strict';
 
     $(function () {
-        $.getJSON('../scripts/colours.json', function (data) {
-            var items = [];
-            $.each(data, function (key, val) {
-                items.push('<div class="col-md-3"><div class="colour"><a href="#'
-                    + key.toLowerCase().split(' ').join('-')
-                    + '" data-key="' + key + '" data-value="' + val
-                    + '"><span class="circle" style="background-color: '
-                    + val + ';"></span> ' + key + '</a></div></div>');
-            });
-            $('.colours').append(items.join(""));
+
+        $.getJSON('scripts/colours.json', function (data) {
+            for (var property in data) {
+                if (data.hasOwnProperty(property)) {
+                    appendColour(property, data[property]);
+                }
+            }
         });
 
-        $('.colours').on('click', 'a', function (e) {
-            e.preventDefault();
-            var key = $(this).data('key');
-            var val = $(this).data('value');
-            var display = $('.large-display');
-            var heading = $('.large-display h1');
-            display.removeAttr('style').css('background-color', val);
-            heading.text(key);
-
-            display.removeClass('hide');
-        });
     });
+
+
+    var appendColour = function (name, colour) {
+        var text = $('<p />').text(name);
+        var shadow = $('<div />').addClass('shadow').append(text);
+        var box = $('<div />').addClass('colour').css('background-color', colour).append(shadow);
+
+        box.appendTo('.colours');
+    }
 })();
